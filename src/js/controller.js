705feb9edc9e473c26d9fa41be0ea61e1defcf78
idreez <idreezyusuf@yahoo.com>
2024-04-20 21:04:03 +0100

    fixed bug at master

--- src/js/controller.js
+++ src/js/controller.js
@@ -135,7 +135,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   console.log('Welcome!');
-  alert('hacked')
 };
 
 init();
